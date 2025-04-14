8a4392fc6affb2a5328219e80c0d75d06050ff6d
NathanielM-04 <nathanielmamucod@gmail.com>
2025-04-14 19:35:32 +0800

    app,data

--- appleave_project/app/Config/Database.php
+++ appleave_project/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => ' 192.168.100.7',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'hr_database',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
